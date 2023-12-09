9bb3e3247a576da94fff7c1f34b2167e25a92aad
jorg195 <jorgeam96@hotmail.com>
2023-12-08 23:39:09 -0600

    Implementing service domain logic, added DTO's, ports, mappers

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/mapper/OrderDataMapper.java
@@ -0,0 +1,68 @@
+package com.food.ordering.system.order.service.domain.mapper;
+
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.domain.valueobject.Money;
+import com.food.ordering.system.domain.valueobject.ProductId;
+import com.food.ordering.system.domain.valueobject.RestaurantId;
+import com.food.ordering.system.order.domain.entity.Order;
+import com.food.ordering.system.order.domain.entity.Product;
+import com.food.ordering.system.order.domain.entity.Restaurant;
+import com.food.ordering.system.order.domain.valueobject.StreetAddress;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderCommand;
+import com.food.ordering.system.order.service.domain.dto.create.CreateOrderResponse;
+import com.food.ordering.system.order.service.domain.dto.create.OrderAddress;
+import com.food.ordering.system.order.service.domain.dto.create.OrderItem;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class OrderDataMapper {
+
+    public Restaurant createOrderCommandToRestaurant(CreateOrderCommand createOrderCommand){
+        return Restaurant.builder().id(new RestaurantId(createOrderCommand.getRestaurantId()))
+                .products(createOrderCommand.getItems().stream().map(orderItem ->
+                        new Product(new ProductId(orderItem.getProductId()))).collect(Collectors.toList()))
+                .build();
+    }
+
+    public Order createOrderCommandToOrder(CreateOrderCommand createOrderCommand){
+        return Order.builder()
+                .customerId(new CustomerId(createOrderCommand.getCustomerId()))
+                .restaurantId(new RestaurantId(createOrderCommand.getRestaurantId()))
+                .deliveryAddress(orderAddressToStreetAddress(createOrderCommand.getAddress()))
+                .price(new Money(createOrderCommand.getPrice()))
+                .items(orderItemsToOrderItemEntities(createOrderCommand.getItems()))
+                .build();
+    }
+
+    private StreetAddress orderAddressToStreetAddress(OrderAddress orderAddress){
+        return new StreetAddress(
+                UUID.randomUUID(),
+                orderAddress.getStreet(),
+                orderAddress.getPostalCode(),
+                orderAddress.getCity()
+        );
+    }
+
+    private List<com.food.ordering.system.order.domain.entity.OrderItem> orderItemsToOrderItemEntities (List<OrderItem> orderItems){
+        return orderItems.stream()
+                .map(orderItem ->
+                        com.food.ordering.system.order.domain.entity.OrderItem.builder()
+                                .product(new Product(new ProductId(orderItem.getProductId())))
+                                .price(new Money(orderItem.getPrice()))
+                                .quantity(orderItem.getQuantity())
+                                .subTotal(new Money(orderItem.getSubTotal()))
+                                .build()).collect(Collectors.toList());
+    }
+
+    public CreateOrderResponse orderToCreateOrderResponse(Order order){
+        return CreateOrderResponse.builder()
+                .orderTrackingId(order.getTrackingId().getValue())
+                .orderStatus(order.getOrderStatus())
+                .build();
+    }
+
+}
--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/domain/entity/Product.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/domain/entity/Product.java
@@ -14,11 +14,20 @@ public class Product extends BaseEntity<ProductId> {
         this.price = price;
     }
 
+    public Product(ProductId productId){
+        super.setId(productId);
+    }
+
     public String getName() {
         return name;
     }
 
     public Money getPrice() {
         return price;
+    }
+
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
+        this.name = name;
+        this.price = price;
     }
 }
